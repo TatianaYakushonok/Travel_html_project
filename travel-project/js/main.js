75d482a84320de02a1ba50fdf7ab449cbf638a08
TatianaYakushonok <yakush2@yandex.ru>
2022-07-17 23:14:04 +0700

    feat: add adaptive for section header (Sun, 17 Jul 2022 23:14:03 +0700)

--- travel-project/js/main.js
+++ travel-project/js/main.js
@@ -1,4 +1,6 @@
 
+//================= owl-carousel ====================
+
 const owl = $('.owl-carousel');
 owl.owlCarousel({
     center: true,
@@ -15,3 +17,16 @@ $('.slider__btn--prev').click(function() {
 $('.slider__btn--next').click(function() {
     owl.trigger('next.owl.carousel');
 })
+
+//================ burger menu =======================
+
+const navBtn = document.querySelector('.nav__toggle');
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+
+
+navBtn.onclick = function(){
+    menuIcon.classList.toggle('menu-icon-active');
+    nav.classList.toggle('nav__mobile');
+    document.body.classList.toggle('no-scroll');
+}
